85218772e9220e60b67284e0bcd44a68b2160d64
DSjayjang <a52071194@gmail.com>
2025-05-09 18:19:55 +0900

    Update add.py

--- add.py
+++ add.py
@@ -1,48 +1,28 @@
 # %%
-import os
-os.environ['R_HOME'] = r'C:\Programming\R\R-4.4.2'
-import rpy2
-print(rpy2.__version__)
-
-# %%
-from rpy2.robjects.packages import importr
-from rpy2.robjects import numpy2ri
-import numpy as np
-
-# %%
-print(r['R.version.string'][0])
-print(rpy2.__version__)
-# %%
-
-from rpy2.robjects import r
-import rpy2.robjects as robjects
-r.source('test.R')
-my_add = robjects.globalenv['my_add']
-my_add(1, 2)
-
-# %%
-
-# %%
+# rpy2 및 환경변수 설정
 import os
 os.environ['R_HOME'] = r'C:\Programming\R\R-4.4.2'
 
 import rpy2
-print(rpy2.__version__)
+print('rpy2 version:', rpy2.__version__)
 
 # %%
 from rpy2.robjects import r
-import rpy2.robjects as robjects
 
 # cusum load
 r_path = os.path.abspath(r'C:\Programming\Github\ADD\Cusum')
 r.source(r_path + '\cusum_fitting_share.R')
-# %%
 
-print(r.ls())
+print('R 객체 리스트:\n', r.ls())
+
 # %%
 start_point = r['start.point']
-time_labels = r['time.labels'] 
-print(start_point)
-print(time_labels[start_point])
+time_labels = r['time.labels']
 
+detect_time = time_labels[start_point[0]]
+
+print('start_point:', start_point[0])
+print('detect_time:', detect_time)
+# %%
+r("print(start.point)")
 # %%
