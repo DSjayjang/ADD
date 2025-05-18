19274f67139b26c622a0ca8760e5494442537360
DSjayjang <a52071194@gmail.com>
2025-05-18 19:18:12 +0900

    cusum

--- add.py
+++ add.py
@@ -1,22 +1,20 @@
-# %%
 import pandas as pd
 import numpy as np
-import os
-os.environ['R_HOME'] = r'C:\Programming\R\R-4.4.2'
-
-import rpy2
-from rpy2.robjects import r
-
-
-print('rpy2 version:', rpy2.__version__)
 
 # 환경변수 설정
-# PCRL PC
-r_path = os.path.abspath(r'C:\Programming\Github\ADD')
+## 설치된 R 경로 설정
+import os
+os.environ['R_HOME'] = r'C:\Programming\R\R-4.4.2' # PCRL
+# os.environ['R_HOME'] = r'C:\Programming\R\R-4.3.3' # laptop
 
-# # My labtop
-# os.environ['R_HOME'] = r'C:\Programming\R\R-4.3.3'
-# r_path = os.path.abspath(r'C:\Programming\Git\Github\ADD')
+# rpy2
+import rpy2
+from rpy2.robjects import r
+print('rpy2 version:', rpy2.__version__)
+
+# PCRL PC
+r_path = os.path.abspath(r'C:\Programming\Github\ADD') # PCRL
+# r_path = os.path.abspath(r'C:\Programming\Git\Github\ADD') # laptop
 
 # cusum load
 r.source(r_path + '\Cusum\cusum_fitting_share.R')
@@ -26,6 +24,8 @@ start_point = r['start.point']
 time_labels = r['time.labels']
 detect_time = time_labels[start_point[0]] # type: str
 
+# 탐지시간 출력
+# type: str
 print('start_point:', start_point[0])
 print('detect_time:', detect_time)
 
@@ -34,4 +34,3 @@ r.source(r_path + '\Wavelet\ADD_function.R')
 print('R 객체 리스트:\n', r.ls())
 
 r['res_mean1']
-# %%
--- mycusum.py
+++ mycusum.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class uniCUSUM:
-    def __init__(self, phase1_len=100, h=5, delta=0.5):
+    def __init__(self, phase1_len=100, threshold=5, delta=0.5):
         self._delta = float(delta)
-        self._h = float(h)
+        self._threshold = float(threshold)
         self._phase1_len = phase1_len
 
         # 초기값
@@ -15,13 +15,17 @@ class uniCUSUM:
         self.violation_upper = []
         self.violation_lower = []   
 
-    def fit(self, phase1_X: np.array):
-        # Phase 1
-        self.phase1_data = phase1_X[:self._phase1_len]
-        self._mu0 = np.mean(self.phase1_data)
-        self._sigma = np.std(self.phase1_data, ddof = 1)
+    # Phase 1
+    def fit(self, X: np.ndarray):
+        phase1_data = X[:self._phase1_len]
+
+        self._mu0 = np.mean(phase1_data)
+        self._sigma = np.std(phase1_data, ddof = 1)
     
-    def update(self, phase2_X):        
+    # Phase 2
+    def update(self, X: np.ndarray):
+        phase2_X = X[self._phase1_len:]
+
         for idx, x in enumerate(phase2_X):
             s_t_pos = max(0.0, self._s_prev_pos + (x - self._mu0 - self._delta/2) / self._sigma)
             s_t_neg = max(0.0, self._s_prev_neg + (self._mu0 - x - self._delta/2) / self._sigma)
@@ -30,19 +34,59 @@ class uniCUSUM:
             self._s_prev_pos = s_t_pos
             self._s_prev_neg = s_t_neg
 
-            if self._s_prev_pos > self._h:
+            if self._s_prev_pos > self._threshold:
                 start_idx = self._phase1_len + idx
                 self.violation_upper.append(start_idx)
-                # self.reset()
 
-            if self._s_prev_neg > self._h:
+            if self._s_prev_neg > self._threshold:
                 start_idx = self._phase1_len + idx
                 self.violation_lower.append(start_idx)
-                # self.reset()
 
         return self.violation_lower, self.violation_upper
 
-    # def reset(self):
-    #     # 벡터 누적합 리셋
-    #     self._s_prev_pos = 0.0
-    #     self._s_prev_neg = 0.0
+class multiCUSUM:
+    def __init__(self, phase1_len=100, threshold=5, k=1.0):
+        self._k = float(k)
+        self._threshold = float(threshold)
+        self._phase1_len = phase1_len
+
+        # 초기값
+        self._mu0 = []
+        self._cov0 = []
+        self.violation = []
+
+    # Phase 1
+    def fit(self, X: np.ndarray):
+        X = np.asarray(X, dtype=float)
+        phase1_data = X[:self._phase1_len, :]
+
+        self._mu0 = np.mean(phase1_data, axis = 0)
+        self._cov0 = np.cov(phase1_data, rowvar = False)
+
+    # Phase 2
+    def update(self, X: np.ndarray):
+        X = np.asarray(X, dtype=float)
+        phase2_X = X[self._phase1_len:, :]
+
+        self.n_t = 1
+        self.C_t = np.zeros(phase2_X.shape[1]) # 초기 누적합 벡터
+        self._inv_cov0 = np.linalg.inv(self._cov0)
+
+        for t in range(self._phase1_len, phase2_X.shape[0]):
+            # 시점 t에서의 누적합 벡터
+            window = X[t-self.n_t+1 : t+1]
+            C_t = np.sum(window - self._mu0, axis = 0)
+
+            # 탐지 통계량
+            stat = C_t @ self._inv_cov0 @ C_t.T
+            MC_t = max(0, np.sqrt(stat) - self._k * self.n_t)
+
+            if MC_t > self._threshold:
+                start_idx = self._phase1_len + t
+                self.violation.append(start_idx)
+
+            # 다음 시점의 n_t 갱신
+            if MC_t > 0: self.n_t += 1
+            else: self.n_t = 1
+
+        return self.violation
